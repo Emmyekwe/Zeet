e7d81dad8c23fb5b222916e76a5e2bf5e9ca9c21
Emmanuel Ukhueleigbe <Emmanuel_Ukhueleigbe@SAM.local>
2020-09-15 00:32:32 +0100

    Updates

---
+++ precache-manifest.232c478f91d0848669446992f4d9250a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874a015d5698a1d68abfa535d16563a9",
+    "url": "/Zeet/index.html"
+  },
+  {
+    "revision": "a3395c5e2a35d72929e2",
+    "url": "/Zeet/static/css/main.57f25316.chunk.css"
+  },
+  {
+    "revision": "5f45b932ccdb24625ff6",
+    "url": "/Zeet/static/js/2.6183e824.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zeet/static/js/2.6183e824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3395c5e2a35d72929e2",
+    "url": "/Zeet/static/js/main.d09857f5.chunk.js"
+  },
+  {
+    "revision": "e096adaf8808cf7647d1",
+    "url": "/Zeet/static/js/runtime-main.513778d4.js"
+  }
+]);
